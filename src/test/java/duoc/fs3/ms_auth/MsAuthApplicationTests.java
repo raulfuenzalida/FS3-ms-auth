1e8dd9193f5fc0fb12a423695a9f384ff1e4dc89
Raúl Fuenzalida <rau.fuenzalida@duocuc.cl>
2026-04-16 09:39:25 -0400

    ADD: inicializa microservicio ms-auth con configuración base de Spring Boot

---
+++ src/test/java/duoc/fs3/ms_auth/MsAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package duoc.fs3.ms_auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
