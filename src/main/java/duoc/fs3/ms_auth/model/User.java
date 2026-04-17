a378bf897d91923084e73009203b2426480ad164
Raúl Fuenzalida <rau.fuenzalida@duocuc.cl>
2026-04-17 14:58:05 -0400

    feat: add User model y repository

---
+++ src/main/java/duoc/fs3/ms_auth/model/User.java
@@ -0,0 +1,26 @@
+package duoc.fs3.ms_auth.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
